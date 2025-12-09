33a4319981c093211e912a2fcc1fe21afe640a46
Victor Sheyanov <1764195+vsheyanov@users.noreply.github.com>
2025-12-09 15:07:58 +0100

    chore(GDD-7077): fixed example

--- examples/test_app/fixtures.tf
+++ examples/test_app/fixtures.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  secret_base_path = "arn:aws:secretsmanager:${data.aws_region.current.name}:${var.data.aws_caller_identity.current.account_id}:secret:/applications/demoapp"
+  secret_base_path = "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:/applications/demoapp"
 }
 
 data "aws_iam_policy_document" "onboarding_lambdas_read_credentials" {
