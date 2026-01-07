1e57ba8476b694ab113909e1b5fc4f7d66c3114d
Victor Sheyanov <1764195+vsheyanov@users.noreply.github.com>
2026-01-07 16:28:56 +0100

    chore(GDD-7251): only export role

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,4 @@
-output "aws_iam_policies" {
-  value = aws_iam_policy.policies
-}
 
 output "aws_iam_role" {
   value = aws_iam_role.role
-}
-
-output "role_arn" {
-  value = aws_iam_role.role.arn
-}
-
-output "role_name" {
-  value = aws_iam_role.role.name
-}
-
-output "debug_policy_docs" {
-  value = var.source_policy_documents
 }
