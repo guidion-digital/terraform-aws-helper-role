9b0111c4934f34551ad498cf24fbb98ecb4d717b
Victor Sheyanov <1764195+vsheyanov@users.noreply.github.com>
2026-01-07 15:19:46 +0100

    chore(GDD-7251): fixed validation

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "source_policy_documents" {
   validation {
     condition = alltrue([
       for doc in var.source_policy_documents :
-      can(jsondecode(doc).Version) && can(jsondecode(doc).Statement)
+      can(doc.Version) && can(doc.Statement)
     ])
     error_message = "Each source policy document must be a valid IAM policy JSON with Version and Statement keys."
   }
